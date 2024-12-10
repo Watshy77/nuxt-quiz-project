f41b4ff33ee9d38247588223d162d8c44354c199
William Queva <watshy77@gmail.com>
2024-12-10 14:29:04 +0100

    add style for error page

--- pages/error.vue
+++ pages/error.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        <h1>Quiz introuvable</h1>
-        <p><router-link to="/">Retour à la page principale</router-link></p>
+    <div class="flex flex-col items-center justify-center">
+        <h1 class="text-4xl font-bold text-red-500 mb-4">Quiz introuvable</h1>
+        <p>
+            <router-link to="/" class="text-blue-500 hover:underline">
+                Retour à la page principale
+            </router-link>
+        </p>
     </div>
 </template>
